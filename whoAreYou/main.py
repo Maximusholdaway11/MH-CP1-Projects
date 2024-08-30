bc162f81530879dc05d53bbe2fd34478eeddb421
Holdaway <max.holdaway@npgdom.com>
2024-08-30 10:49:05 -0600

    Who are you assignment

---
+++ whoAreYou/main.py
@@ -0,0 +1,9 @@
+#Max Holdaway Who are you?
+
+indivi = input("What is your name?: ")
+
+aged = input("How old are you?: ")
+
+coloring = input("What is your favorite color?: ")
+
+print("Hello", indivi,"Your age is", aged,"and I do know your favorite color its", coloring)
