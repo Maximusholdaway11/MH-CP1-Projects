b341d38451a46cf8635fa10dd24ac494b1137abb
Holdaway <max.holdaway@npgdom.com>
2024-09-05 10:25:55 -0600

    New Celsius to Fahrenheit project done

---
+++ Celsius to Fahrenheit/main.py
@@ -0,0 +1,9 @@
+#Max Holdaway Celsius to Fahrenheit profficency test
+
+celsius = int(input("Enter temperature in celsius:"))
+
+fahrenheit = (celsius*(9/5))+32
+
+str(print("This is temperature in celsius:", celsius))
+
+str(print("This is temperature in fahrenheit:", fahrenheit))
