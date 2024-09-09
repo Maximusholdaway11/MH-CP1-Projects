c40d52ced698ed619ab102e8c0d46e92d2c8c423
Holdaway <max.holdaway@npgdom.com>
2024-09-09 10:36:20 -0600

    Saved Assignment (forgot to before)

--- Setting, Resetting, and Resetting/main.py
+++ Setting, Resetting, and Resetting/main.py
@@ -1,10 +1,13 @@
-#Max Holdaway awards tables
+#Max Holdaway Setting, Resetting, and Resetting Skill Practice
+SchoolBoard = 1
 staff = 32
+staff = staff - 3
 students = 100
 students = students -1
-guests = students*2
+guests = (students*2)
+guests = guests - 17
 seats = 12
 
 
 
-print((staff+students+guests)/seats)
+print((staff+students+guests+SchoolBoard)/seats)
