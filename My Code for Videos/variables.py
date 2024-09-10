75035d36e993c8ea46d5a70a0ee7a41e1681503c
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-09-10 18:16:32 +0000

    Made more code for ex

--- My Code for Videos/variables.py
+++ My Code for Videos/variables.py
@@ -1,7 +1,22 @@
 #Max Holdaway Variables Infographic Ex Code
 
-ExVar = 10
+#Message to show what program does
+print("this is a three term multiplacation only calculator (excluding variables like x and y)")
 
-ExVar = 20
+#Making space between the message and the input
+print("")
 
-print(ExVar)
+#Asking to input first term in equation
+FTerm = int(input("Please type the first term: "))
+
+#Asking to input second term in euqation
+STerm = int(input("Please type the second term: "))
+
+#Asking to input third term in equation
+ThTerm = int(input("Please type the third term: "))
+
+#Using the three terms and multiplying them
+SimTerm = FTerm*STerm*ThTerm
+
+#Showing the user the result
+print(f"Your answer is: {SimTerm}")
