ed15810b36d89521c92bb7768bcacfdf1d0da576
Holdaway <max.holdaway@npgdom.com>
2024-09-12 10:28:24 -0600

    Madlib assignment

---
+++ Madlib/main.py
@@ -0,0 +1,28 @@
+#Max Holdaway Madlib
+
+#The title message explaining what is going on to the person who runs the code and uses it
+print("This is a madlib game you will be given five prompts for words just put in anything you can think of that might be funny.")
+
+#Adding a space between the input and title
+print("")
+
+#Getting the first word used
+Word1 = input("Give me a word to use: ")
+
+#Getting the second word used
+Word2 = input("Give me a second word to use: ")
+
+#Getting the third word used
+Word3 = input("Give me a third word to use: ")
+
+#Getting the fourth word used
+Word4 = input("Give me a fourth word to use: ")
+
+#Getting the fifth word use
+Word5 = input("Give me a fifth word to use: ")
+
+#Adding another space between inputs and story
+print("")
+
+#Writing out the story for the user to see
+print(f"In a normal city {Word1} lived a peaceful live. Then all of the sudden {Word2} appeared and things began to change! {Word2} started to make {Word3} appear randomly! They also had to make {Word4} appear. In the end {Word5} put an end to {Word2} and {Word1} continued livingapp normally.")
