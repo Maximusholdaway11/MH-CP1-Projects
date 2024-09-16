01f1667548c557b61ae12bb1c02b9b3a0b162f31
Holdaway <max.holdaway@npgdom.com>
2024-09-16 10:51:35 -0600

    Worked More

--- Numbers profficency test/main.py
+++ Numbers profficency test/main.py
@@ -2,6 +2,8 @@
 
 print("This is an arithmetic only calculator with a few exceptions namely: Modulo, Exponents, and Rounded Division")
 
+print("")
+
 NumberTerm1 = float(input("Type the first number to be used: "))
 
 NumberTerm2 = float(input("Type the second number to be used: "))
@@ -30,7 +32,29 @@ def Division(x, y):
 
 DividedProduct = Division(NumberTerm1, NumberTerm2)
 
-Arithmetic = int(input("Tell me which type of arithmetic you want to calculate or which execption, Addition is 1, Subtraction is 2, and Multiplication is 3, Division is 4, : "))
+def Exponents(x, y):
+    z = x ** y
+    return z
+
+ExponentProduct = Exponents(NumberTerm1, NumberTerm2)
+
+def ModularDivision(x, y,):
+    z = x % y
+    return z
+
+ModularProduct = ModularDivision(NumberTerm1, NumberTerm2)
+
+def QuotientDivision(x, y):
+    z = x // y
+    return z
+
+QuotientProduct = QuotientDivision(NumberTerm1, NumberTerm2)
+
+Arithmetic = int(input("Tell me which type of arithmetic you want to calculate or which execption, Addition is 1, Subtraction is 2, and Multiplication is 3, Division is 4, Exponents is 5, Modular division is 6, and Quotient Division is 7: "))
+
+print("")
+
+round(DividedProduct, 2)
 
 if Arithmetic == 1:
     print(f"{Solution}")
@@ -39,7 +63,13 @@ elif Arithmetic == 2:
 elif Arithmetic == 3:
     print(f"{Product}")
 elif Arithmetic == 4:
-    print(f"{DividedProduct}")
+    print(round(DividedProduct, 2))
+elif Arithmetic == 5:
+    print(f"{ExponentProduct}")
+elif Arithmetic == 6:
+    print(f"{ModularProduct}")
+elif Arithmetic == 7:
+    print(f"{QuotientProduct}")
 
 
 
