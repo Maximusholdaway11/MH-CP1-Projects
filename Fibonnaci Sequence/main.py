960ee6978cc2c6df4b46e8afee99d0fa49ae5dcd
Holdaway <max.holdaway@npgdom.com>
2024-09-17 10:51:10 -0600

    Saving progress

---
+++ Fibonnaci Sequence/main.py
@@ -0,0 +1,10 @@
+#Max Holdaway Fibonnaci Sequence
+
+print("This is a Fibonnaci Sequence Calculator.")
+
+StartingValue = int(input("Please give me a starting value for the sequence: "))
+
+while StartingValue < 100:
+    if StartingValue == StartingValue:
+        StartingValue + 1
+        print(StartingValue)
