3611955938e7c2607e4a9ca19a6e13d7a486b9a6
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-09-23 18:17:57 +0000

    Big progress on pig latin converter

--- Pig Latin Converter/main.py
+++ Pig Latin Converter/main.py
@@ -1,17 +1,19 @@
 #Max Holdaway Pig Latin Converter
 
-print("This is a pig latin converter it translates one word of english to pig latin.")
+print("This is a pig latin converter it converts english to pig latin each input is a different word translated.")
 
 print("")
 
 EnglishWord = str(input("Please give me a word to translate into pig latin: "))
 
-def PigTranslator(EnglishWord, PigLatinWord, SplitEnglishWord):
+EnglishWord2 = str(input("Please give me a second word to translate into pig latin: "))
+
+def PigConverter(EnglishWord, PigLatinWord, SplitEnglishLetter, SplitEnglishWord):
     SplitEnglishWord = EnglishWord[1:5]
-
+    SplitEnglishLetter = EnglishWord[0:1]
+    SplitEnglishLetter = SplitEnglishLetter + "ay"
+    EnglishWord = SplitEnglishWord + "-" + SplitEnglishLetter
     PigLatinWord = EnglishWord
-    print(PigLatinWord)
+    return PigLatinWord
 
-PigLatin = ""
-
-PigTranslator(EnglishWord, PigLatin)
+print(PigConverter(EnglishWord, "", "", ""), PigConverter(EnglishWord2, "", "", ""))
