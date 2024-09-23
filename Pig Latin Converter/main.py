3e6c54d6ccb9ff0b13fcda05247e1554278fc110
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-09-23 21:37:29 +0000

    Completed Pig Latin Converter

--- Pig Latin Converter/main.py
+++ Pig Latin Converter/main.py
@@ -4,16 +4,48 @@ print("This is a pig latin converter it converts english to pig latin each input
 
 print("")
 
-EnglishWord = str(input("Please give me a word to translate into pig latin: "))
+EnglishWord1 = str(input("Please give me a word to translate into pig latin: "))
 
 EnglishWord2 = str(input("Please give me a second word to translate into pig latin: "))
+
+EnglishWord3 = str(input("Please give me a third word to translate into pig latin: "))
+
+print("")
+
+TypeOfWord1 = (input("Is the first word start with A, E, I, O, or U? if so say True if not say False (Use uppercase for true or false): "))
+
+TypeOfWord2 = (input("Is the second word start with A, E, I, O, or U? if so say True if not say False (Use uppercase for true or false): "))
+
+TypeOfWord3 = (input("Is the third word start with A, E, I, O, or U? if so say True if not say False (Use uppercase for true or false): "))
 
 def PigConverter(EnglishWord, PigLatinWord, SplitEnglishLetter, SplitEnglishWord):
     SplitEnglishWord = EnglishWord[1:5]
     SplitEnglishLetter = EnglishWord[0:1]
     SplitEnglishLetter = SplitEnglishLetter + "ay"
-    EnglishWord = SplitEnglishWord + "-" + SplitEnglishLetter
+    EnglishWord = SplitEnglishWord + SplitEnglishLetter
     PigLatinWord = EnglishWord
     return PigLatinWord
 
-print(PigConverter(EnglishWord, "", "", ""), PigConverter(EnglishWord2, "", "", ""))
+def PigConverterAEIOU(EnglishWord, PigLatinWord):
+    EnglishWord = EnglishWord + "ay"
+    PigLatinWord = EnglishWord
+    return PigLatinWord
+
+print("")
+
+if TypeOfWord1 == "True":
+    PigLatin1 = PigConverterAEIOU(EnglishWord1, "")
+elif TypeOfWord1 == "False":
+    PigLatin1 = PigConverter(EnglishWord1, "", "", "")
+
+if TypeOfWord2 == "True":
+    PigLatin2 = PigConverterAEIOU(EnglishWord2, "")
+elif TypeOfWord2 == "False":
+    PigLatin2 = PigConverter(EnglishWord2, "", "", "")
+
+if TypeOfWord3 == "True":
+    PigLatin3 = PigConverterAEIOU(EnglishWord3, "")
+elif TypeOfWord3 == "False":
+    PigLatin3 = PigConverter(EnglishWord3, "", "", "")
+
+print(PigLatin1, PigLatin2, PigLatin3)
