e00d0e48b5645d34dc34d7529845ae036671c05c
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-09-24 21:34:39 +0000

    Even more progress on Shift Cipher

--- Shift Cipher/main.py
+++ Shift Cipher/main.py
@@ -6,64 +6,16 @@ print("")
 
 UnCryptedMessage = input("Please give me a message to encrypt: ")
 
-UnCryptedMessage = [UnCryptedMessage]
+ShiftingNumber = int(input("Please give me the number of times you want to shift your message for encrypting: "))
 
 print("")
 
-a = "a"
+MessageList = []
 
-b = "b"
+def ShiftCrypting(Message, UnCryptedMessageVar):
+    for character in Message:
+        x = ord(character)
+        x = x + ShiftingNumber
+        UnCryptedMessageVar.append(chr(x))
+    return UnCryptedMessageVar
 
-c = "c"
-
-d = "d"
-
-e = "e"
-
-f = "f"
-
-g = "g"
-
-h = "h"
-
-i = "i"
-
-j = "j"
-
-k = "j"
-
-l = "l"
-
-m = "m"
-
-n = "n"
-
-o = "o"
-
-p = "p"
-
-q = "q"
-
-r = "r"
-
-s = "s"
-
-t = "t"
-
-u = "u"
-
-v = "v"
-
-w = "w"
-
-x = "x"
-
-y = "y"
-
-z = "z"
-
-def ShiftCrypting(Message, CryptedMessage, SemiCryptedMessage, CharList):
-    CharList = (a.ord, b.ord, c.ord, d.ord, e.ord, f.ord, g.ord, h.ord, i.ord, j.ord, k,ord, l.ord, m.ord, n.ord, o.ord, p.ord, q.ord, r.ord, s.ord, t.ord, u.ord, v.ord, w.ord, x.ord, y.ord, z.ord)
-    CryptedMessage = Message
-
-print(ShiftCrypting(UnCryptedMessage, "", ""))
