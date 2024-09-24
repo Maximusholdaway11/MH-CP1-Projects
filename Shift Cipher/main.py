1750beebed438df25d66df50e5802f1ba260def6
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-09-24 18:15:46 +0000

    Made some more changesto Shift Cipher

--- Shift Cipher/main.py
+++ Shift Cipher/main.py
@@ -6,6 +6,8 @@ print("")
 
 UnCryptedMessage = input("Please give me a message to encrypt: ")
 
+UnCryptedMessage = [UnCryptedMessage]
+
 print("")
 
 a = "a"
@@ -20,8 +22,48 @@ e = "e"
 
 f = "f"
 
+g = "g"
+
+h = "h"
+
+i = "i"
+
+j = "j"
+
+k = "j"
+
+l = "l"
+
+m = "m"
+
+n = "n"
+
+o = "o"
+
+p = "p"
+
+q = "q"
+
+r = "r"
+
+s = "s"
+
+t = "t"
+
+u = "u"
+
+v = "v"
+
+w = "w"
+
+x = "x"
+
+y = "y"
+
+z = "z"
+
 def ShiftCrypting(Message, CryptedMessage, SemiCryptedMessage, CharList):
-    CharList = (a.ord, b.ord, c.ord, d.ord, e.ord, f.ord)
-    CryptedMessage = CharList
+    CharList = (a.ord, b.ord, c.ord, d.ord, e.ord, f.ord, g.ord, h.ord, i.ord, j.ord, k,ord, l.ord, m.ord, n.ord, o.ord, p.ord, q.ord, r.ord, s.ord, t.ord, u.ord, v.ord, w.ord, x.ord, y.ord, z.ord)
+    CryptedMessage = Message
 
 print(ShiftCrypting(UnCryptedMessage, "", ""))
