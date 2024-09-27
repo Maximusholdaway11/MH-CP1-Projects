f6760d9666dcd9e96947c37832b7d1c5d44011ca
Holdaway <max.holdaway@npgdom.com>
2024-09-27 10:50:44 -0600

    Palindrom Assignment

---
+++ Palindrom/main.py
@@ -0,0 +1,25 @@
+#Max Holdaway Palindrom
+
+print("This is a palindrom checker.")
+
+WordBeingChecked = input("Please give me a word to be checked: ")
+
+if WordBeingChecked == "Mom" or "mom":
+    print("This is a palindrom")
+else:
+    print("This isn't a palindrom")
+
+if WordBeingChecked == "Dad" or "dad":
+    print("This is a palindrom")
+elif WordBeingChecked == "a" or "A":
+    print("This is a palindrom")
+elif WordBeingChecked == "Racecar" or "racecar":
+    print("This is a palindrom")
+elif WordBeingChecked == "Civic" or "civic":
+    print("This is a palindrom")
+elif WordBeingChecked == "Step on no pets" or "step on no pets":
+    print("This is a palindrom")
+elif WordBeingChecked == "Borrow or rob" or "borrow or rob":
+    print("This is a palindrom")
+elif WordBeingChecked == "Radar" or "radar":
+    print("This is a palindrom")
