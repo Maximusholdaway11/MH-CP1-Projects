53b7364c68e3afdb0984c59e7a0efa6ed0514c51
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-01 18:13:57 +0000

    Finished Area Calculator

--- Area Calculator/main.py
+++ Area Calculator/main.py
@@ -2,36 +2,41 @@
 
 import math
 
-print("This is an area calculator it calculates the area of a Square, Rectangle, Triangle, Circle, And Trapezoid.")
+print("This is an area calculator it calculates the area of Squares, Rectangles, Triangles, Circles, And Trapezoids.")
 
 print("")
 
-FirstLength = input("Please give me the Sidelength of Square / Rectangle Length / Triangle Base / Radius of circle / First Base of Trapezoid: ")
+FirstLength = float(input("Please give me the Sidelength of Square / Rectangle Length / Triangle Base / Radius of circle / First Base of Trapezoid: "))
 
-SecondLength = input("Please give me the Rectangle Width / Triangle Height / Second Base of Trapezoid (Even if your only calulating a sqaure): ")
+SecondLength = float(input("Please give me the Rectangle Width / Triangle Height / Second Base of Trapezoid (Even if your only calulating a sqaure): "))
 
-ThirdLength = input("Please give me the height of Trapezoid (Even if your not using the trapezoid area calc or Calculating a sqaure): ")
+ThirdLength = float(input("Please give me the height of Trapezoid (Even if your not using the trapezoid area calc or Calculating a sqaure): "))
 
 pi = math.pi
 
 def AreaOfSqaure(SideLength):
     A = SideLength ** 2
+    float(A)
     return A
 
 def AreaOfRectangle(Length, Width):
     A = Length * Width
+    float(A)
     return A
 
 def AreaOfTriangle(Base, Height):
-    A = 0.5(Base * Height)
+    A = (Base * Height) / 2
+    float(A)
     return A
 
 def AreaOfCircle(Radius):
     A = pi * (Radius ** 2)
+    float(A)
     return A
 
 def AreaOfTrapezoid(Base1, Base2, Height):
     A = ((Base1 + Base2)/2) * Height
+    float(A)
     return A
 
 print("")
@@ -40,14 +45,17 @@ TypeOfArea = input("Please tell me the type of area you want me to calculate: ")
 
 print("")
 
-if TypeOfArea in ["Sqaure", "square"]:
-    print((f"This is the area of your sqaure {AreaOfSqaure(FirstLength)}"))
+if TypeOfArea in ["Square", "square"]:
+    print((f"This is the area of your square {AreaOfSqaure(FirstLength)}"))
 
 if TypeOfArea in ["Rectangle", "rectangle"]:
     print(f"This is the area of your rectangle {AreaOfRectangle(FirstLength, SecondLength)}")
 
 if TypeOfArea in ["Triangle", "triangle"]:
-    print((f"This is the area of your triangle {AreaOfTriangle(FirstLength, SecondLength)}"))
+    print(f"This is the area of your triangle {AreaOfTriangle(FirstLength, SecondLength)}")
 
 if TypeOfArea in ["Circle", "circle"]:
-    print()
+    print(f"This is the area of your circle {AreaOfCircle(FirstLength)}")
+
+if TypeOfArea in ["Trapezoid", "trapezoid"]:
+    print(f"This is the area of your trapezoid {AreaOfTrapezoid(FirstLength, SecondLength, ThirdLength)}")
