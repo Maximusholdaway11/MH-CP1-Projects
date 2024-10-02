1b38d49cb622a8c38cf791ee8d7da51ddfae6c8c
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-02 21:39:34 +0000

    More Progress on Improved Simple Calculator

--- Folder for code im testing/OtherMainSimpleCalculator.py
+++ Folder for code im testing/OtherMainSimpleCalculator.py
@@ -43,8 +43,38 @@ while True:
                    Type Division for division
                    Type Exponent for exponent
                    Type Modular for modular division
-                   Type Quotient for quotient divison\n""")
-    if action in [Addition]:
-        AdditionTerm1 = input("Please give me the first number to add: ")
-        AdditionTerm2 = input("Please give me the second number to add: ")
+                   Type Quotient for quotient divison
+                   Type Exit to exit the calculator\n""")
+    
+    if action in ["Addition", "addition"]:
+        AdditionTerm1 = float(input("Please give me the first number to add: "))
+        AdditionTerm2 = float(input("Please give me the second number to add: "))
         print(f"This is the sum {Addition(AdditionTerm1, AdditionTerm2)}")
+
+    elif action in ["Subtraction", "subtraction"]:
+        SubtractionTerm1 = float(input("Please give me the first number to be subtracted from: "))
+        SubtractionTerm2 = float(input("Please give me the second number to subtract: "))
+        print(f"This is the result of the subtraction {Subtraction(SubtractionTerm1, SubtractionTerm2)}")
+
+    elif action in ["Multiplication", "multiplication"]:
+        MultiplicationTerm1 = float(input("Please give me the first number for multiplication: "))
+        MultiplicationTerm2 = float(input("Please give me the second number for multiplication: "))
+        print(f"This is the product {Multiplication(MultiplicationTerm1, MultiplicationTerm2)}")
+
+    elif action in ["Division", "division"]:
+        DivisionTerm1 = float(input("Please give me the first number for division: "))
+        DivisionTerm2 = float(input("Please give me the second number for division: "))
+        print(f"This is the divided product {Division(DivisionTerm1, DivisionTerm2)}")
+
+    elif action in ["Exponent", "exponent"]:
+        ExponentTerm1 = float(input("Please give me a number to use: "))
+        ExponentTerm2 = float(input("Please give me the exponent for the number: "))
+        print(f"This is your product {Exponents(ExponentTerm1, ExponentTerm2)}")
+
+    elif action in ["Exit", "exit"]:
+        print("")
+        print("Okay Exiting")
+        break
+
+    else:
+        print("Unexpected Error please try again")
