172d0df09f4870fc15bec6c80ec3dd1705b20f02
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-15 18:19:26 +0000

    Authorized assignment progress

--- Authorized/main.py
+++ Authorized/main.py
@@ -2,36 +2,37 @@
 
 print("This is an information vault only authorized users can acess the information this will check if you are authorized.")
 
-AuthorizedList = ["AppleBanana", "OrangeApple", "PineappleCoconut"]
-
 Admin = ["BananaPeach"]
 
 Authorized = False
 
 while True:
     action = input("""Are you an authorized user?
-                   Type 1 to check if you are authorized
-                   Type 2 to look at the information if you are authorized
-                   Type 3 to exit the information vault\n """)
+                   Type Authorization to check if you are authorized
+                   Type Look to look at the information if you are authorized
+                   Type Exit to exit the information vault\n """)
     
-    if action == 1:
+    if action in ["Authorization", "authorization"]:
         AskIfAuthorized = [input("Please type the name of your authorized account name: ")]
-        if AskIfAuthorized in AuthorizedList:
+        if AskIfAuthorized in ["AppleBanana", "OrangeApple", "PineappleCoconut"]:
             print("Authorization successful welcome back to the vault")
             Authorized = True
-        elif AskIfAuthorized in Admin:
+        elif AskIfAuthorized in [Admin]:
             print("Welcome back admim")
             Authorized = True
         else:
             print("You are not authorized to view the information")
             Authorized = False
     
-    if action == 2:
+    elif action in ["Look", "look"]:
         if Authorized == True:
             print("Here is the information currently in the vault: BananaPeach is the admin and the vault is nothing more than a secret to keep that safe.")
         else:
             print("Not authorized try again.")
 
-    if action == 3:
+    elif action in ["exit"]:
         print("Thanks for using our vault see you later.")
         break
+
+    else:
+        print("Unexpected Error please try again")
