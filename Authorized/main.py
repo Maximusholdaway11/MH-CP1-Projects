e735dc1fc01d36c37c6d6bdcbb7ac96b9c95571b
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-16 10:28:17 -0600

    Finished Authorized Assignment

--- Authorized/main.py
+++ Authorized/main.py
@@ -1,34 +1,44 @@
 #Max Holdaway Authorized
 
-print("This is an information vault only authorized users can acess the information this will check if you are authorized.")
+print("This is an information vault only authorized users can acess the information this will check if you are authorized. (The authorized users are AppleBanana, OrangeApple, and PineappleCoconut. The admin is BananaPeach)")
+
+AuthorizedList = ["AppleBanana", "OrangeApple", "PineappleCoconut"]
 
 Admin = ["BananaPeach"]
 
 Authorized = False
 
 while True:
-    action = input("""Are you an authorized user?
+    action = str(input("""Are you an authorized user?
                    Type Authorization to check if you are authorized
                    Type Look to look at the information if you are authorized
-                   Type Exit to exit the information vault\n """)
+                   Type Exit to exit the information vault\n """))
     
+    print("")
+
     if action in ["Authorization", "authorization"]:
-        AskIfAuthorized = [input("Please type the name of your authorized account name: ")]
-        if AskIfAuthorized in ["AppleBanana", "OrangeApple", "PineappleCoconut"]:
+        AskIfAuthorized = str(input("Please type the name of your authorized account name: "))
+
+        if AskIfAuthorized in AuthorizedList:
             print("Authorization successful welcome back to the vault")
             Authorized = True
-        elif AskIfAuthorized in [Admin]:
-            print("Welcome back admim")
+            print("")
+        elif AskIfAuthorized in Admin:
+            print("Welcome back admin")
             Authorized = True
+            print("")
         else:
             print("You are not authorized to view the information")
             Authorized = False
+            print("")
     
     elif action in ["Look", "look"]:
         if Authorized == True:
             print("Here is the information currently in the vault: BananaPeach is the admin and the vault is nothing more than a secret to keep that safe.")
+            print("")
         else:
-            print("Not authorized try again.")
+            print("Not authorized please use our authorization system or you will not be able to see the information. (You also can't see it if you are not authorized)")
+            print("")
 
     elif action in ["exit"]:
         print("Thanks for using our vault see you later.")
@@ -36,3 +46,4 @@ while True:
 
     else:
         print("Unexpected Error please try again")
+        print("")
