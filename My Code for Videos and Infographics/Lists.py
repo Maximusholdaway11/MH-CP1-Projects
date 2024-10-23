49688aca14bccecd193c3ed05bdc1c9d73fe700c
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-23 10:49:36 -0600

    Code for Lists infographic

---
+++ My Code for Videos and Infographics/Lists.py
@@ -0,0 +1,14 @@
+#Max Holdaway Lists Infographic Examples
+
+ExList = ["A list", "Can hold", "More than", "One item", "At a time", 3, 4, 5]
+
+print(ExList)
+
+ExList.append("You can also add more items later")
+
+print(ExList)
+
+#You can also remove items later
+ExList.remove("A list")
+
+print(ExList)
