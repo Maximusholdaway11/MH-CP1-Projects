0b73ddeb13fd416b5a06a6ab8e84e37e64e45c12
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-28 10:38:59 -0600

    Formatting Infographic

---
+++ My Code for Videos and Infographics/formatting.py
@@ -0,0 +1,7 @@
+#Max Holdaway Formatting Infograpic
+
+name = input("Name please: ")
+
+ExText = "Hello {:<8} how are you doing? This is a reallllly long number {:,}".format(name, 10000000000)
+
+print(ExText)
