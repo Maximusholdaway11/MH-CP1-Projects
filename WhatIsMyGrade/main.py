1780dd7341ce735bdaf56bb82de3783d70980fbf
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-28 10:51:21 -0600

    More Progress on grade assignment

--- WhatIsMyGrade/main.py
+++ WhatIsMyGrade/main.py
@@ -2,6 +2,34 @@
 
 print("This is a grade calculator.")
 
+NumberConstant = 0
+
+ClassGradeList = []
+
+def LetterGrader(NumberGrade):
+    if NumberGrade >= 93:
+        return "A"
+
 while True:
     Choice = str(input(""""What do you want to do?
-                       """))
+                       To check letter grades write Grades.
+                       To exit write Exit\n"""))
+    
+    if Choice in ["Grades", "grades"]:
+        while NumberConstant <= 7:
+            NumberConstant += 1
+            Class = ""
+            Class = str(input("Please give me the class you want to use for checking grades: "))
+            ClassGradeList.append(Class)
+            Grade = ""
+            Grade = float(input("Please give me the grade for the class: "))
+            ClassGradeList.append(Grade)
+            print("")
+        print(ClassGradeList)
+
+    elif Choice in ["Exit", "exit"]:
+        print("Thank you for using our grade calculator see you later!")
+        break
+
+    else:
+        print("Unexpected Error try again")
