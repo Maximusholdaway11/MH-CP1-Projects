235116738c17bda3ffbf684bea00bc9e8203c204
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-30 10:51:46 -0600

    Infographics

---
+++ My Code for Videos and Infographics/comments.py
@@ -0,0 +1,8 @@
+#I give the code random
+import random
+
+#I make a variable named num
+num = random.randint(1, 20)
+
+#I write hello world
+print("Hello World", num)
---
+++ My Code for Videos and Infographics/conditionals.py
@@ -0,0 +1,10 @@
+#Max Holdaway Conditionals Infographic
+
+TrueOrFalseVar = False
+
+Question = str(input("What do you want to eat?: "))
+
+if Question in ["apple", "Apple"]:
+    TrueVar = True
+    if TrueVar == True:
+        print("Nice you like apples")
