0acf656d5098af1c0695592e492d8a54e1a1e7e3
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-10-31 10:50:26 -0600

    For Loops Infographic

---
+++ My Code for Videos and Infographics/for_loops.py
@@ -0,0 +1,14 @@
+#Max Holdaway For Loops Infographic
+
+#This can be used to see how many letters are in a word
+ExVar = "hello"
+
+ExVar2 = 1
+
+ExList = []
+
+for x in ExVar:
+    ExList.append(ExVar2)
+    ExVar2 += 1
+
+print(ExList)
