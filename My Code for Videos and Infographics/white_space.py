af9e316bc9e0ef0e07a4c8778b6fbdcfe1bc2b4b
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-11-05 10:50:00 -0700

    White Space Infographic

---
+++ My Code for Videos and Infographics/white_space.py
@@ -0,0 +1,17 @@
+#Max Holdaway White Space Infographic
+
+print("White space is what is above this line of code.")
+
+print("It is mainly used to define functions and conditionals.")
+
+FirstNumber = int(input("Please give me a number: "))
+
+SecondNumber = int(input("Please give me a second number: "))
+
+def AdditionFunction(FirstSum, SecondSum):
+    z = FirstSum + SecondSum
+    return z
+
+print(AdditionFunction(FirstNumber, SecondNumber))
+
+print("The space between the z and the numbers signifying which line you are on is white space.")
