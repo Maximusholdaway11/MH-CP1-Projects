578c1033af96ab14d7b50546a38f95c86d5fac82
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-11-11 10:28:08 -0700

    More Progress on Simple Quiz Game

--- SimpleQuizGame/main.py
+++ SimpleQuizGame/main.py
@@ -6,35 +6,82 @@ TotalScore = 0
 
 Difficulty = ""
 
-StartingQuestion = int(input("""What is 120 * 50
+StartingQuestion = input("""What is 120 * 50
                      A. 6000
                      B. 3000
                      C. 9000
                      D. 12000
-                     Type Your Answer Here: """))
+                     Type Your Answer Here: """)
 
-if StartingQuestion == 6000:
+if StartingQuestion in ["A"]:
     TotalScore += 1
     print("You got it correct!")
     Difficulty = "Normal"
+
     if Difficulty in ["Normal"]:
-        Question2 = int(input("""What is 6 squared plus 4 squared
+        Question2 = input("""What is 6 squared plus 4 squared
                               A. 50
                               B. 48
                               C. 52
                               D. 54
+                              Type Your Answer Here: """)
+        if Question2 in ["C"]:
+            print("You got it correct!")
+            TotalScore += 1
+            Difficulty = "Normal"
+
+            if Difficulty in ["Normal"]:
+                Question6 = input()
+        
+        elif Question2 in ["A", "B", "D"]:
+            print("Sadly you got it wrong but the next question will be easier!")
+            Difficulty = "Slightly Easier"
+
+            if Difficulty in ["Slightly Easier"]:
+                Question3 = int(input("""What is 120 - 70
+                              A. 70
+                              B. 40
+                              C. 60
+                              D. 50
                               Type Your Answer Here: """))
-elif StartingQuestion in [3000, 9000, 12000]:
-    print("Sadly you got it wrong but the next question will be easier!.")
+    else:
+        print("Unexpected Error Try Again")
+
+elif StartingQuestion in ["B", "C", "D"]:
+    print("Sadly you got it wrong but the next question will be easier!")
     Difficulty = "Slightly Easier"
+
     if Difficulty in ["Slightly Easier"]:
-        print("Code Works")
         Question3 = int(input("""What is 120 - 70
                               A. 70
                               B. 40
                               C. 60
                               D. 50
                               Type Your Answer Here: """))
+        if Question3 in ["D"]:
+            print("You got it correct!")
+            TotalScore += 1
+            Difficulty = "Normal"
+
+            if Difficulty in ["Normal"]:
+                Question5 = input("")
+        
+        elif Question3 in ["A", "B", "C"]:
+            print("Sadly you got it wrong but the next question will be easier!")
+            Difficulty = "Way Easier"
+
+            if Difficulty in ["Way Easier"]:
+                Question4 = str(input("""What is 1 + 1?
+                              A. 1
+                              B. 2
+                              C. 3
+                              D. 4
+                              Type Your Answer Here: """))
+    else:
+        print("Unexpected Error Try Again")
+
+else:
+    print("Unexpected Error Try Again")
 
 #Question4 = str(input("""What is 1 + 1?
 #                     A. 1
