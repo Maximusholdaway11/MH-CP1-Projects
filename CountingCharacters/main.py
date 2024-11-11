76b81cc216ef8523a6ecc9331c351b6a22816ac8
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-11-11 10:51:22 -0700

    Counting Characters progress

---
+++ CountingCharacters/main.py
@@ -0,0 +1,27 @@
+#Max Holdaway Counting Characters
+
+ACount = 0
+
+BCount = 0
+
+CCount = 0
+
+DCount = 0
+
+ECount = 0
+
+grid = [
+
+["A", "B", "C", "A", "D"],
+
+["C", "A", "B", "D", "E"],
+
+["A", "D", "C", "E", "A"],
+
+["B", "A", "C", "A", "D"],
+
+["D", "C", "B", "E", "A"] ]
+
+
+
+print(ACount, BCount, CCount, DCount, ECount)
