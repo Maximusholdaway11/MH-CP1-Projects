32a1a3cab9eeea320b486ac107af13998c3ffc64
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-11-12 10:51:18 -0700

    More progress on Simple Quiz Game

--- SimpleQuizGame/main.py
+++ SimpleQuizGame/main.py
@@ -31,19 +31,34 @@ if StartingQuestion in ["A"]:
             Difficulty = "Normal"
 
             if Difficulty in ["Normal"]:
-                Question6 = input()
+                Question6 = str(input("""What is the square root of 12544
+                                      A. 147
+                                      B. 135
+                                      C. 112
+                                      D. 122
+                                      Type Your Answer Here: """))
+                
+                if Question6 in ["C"]:
+                    print("You got it correct!")
+                    TotalScore += 1
+                    Difficulty = "Normal"
         
         elif Question2 in ["A", "B", "D"]:
             print("Sadly you got it wrong but the next question will be easier!")
             Difficulty = "Slightly Easier"
 
             if Difficulty in ["Slightly Easier"]:
-                Question3 = int(input("""What is 120 - 70
+                Question3 = str(input("""What is 120 - 70
                               A. 70
                               B. 40
                               C. 60
                               D. 50
                               Type Your Answer Here: """))
+                
+                if Question3 in ["D"]:
+                    print("You got it correct!")
+                    TotalScore += 1
+                    Difficulty = "Normal"
     else:
         print("Unexpected Error Try Again")
 
@@ -52,7 +67,7 @@ elif StartingQuestion in ["B", "C", "D"]:
     Difficulty = "Slightly Easier"
 
     if Difficulty in ["Slightly Easier"]:
-        Question3 = int(input("""What is 120 - 70
+        Question3 = str(input("""What is 120 - 70
                               A. 70
                               B. 40
                               C. 60
@@ -64,7 +79,12 @@ elif StartingQuestion in ["B", "C", "D"]:
             Difficulty = "Normal"
 
             if Difficulty in ["Normal"]:
-                Question5 = input("")
+                Question5 = str(input("""Solve for x, 2(5x + 5) = 50
+                                      A. 4
+                                      B. 5
+                                      C. 6
+                                      D. 3
+                                      Type Your Answer Here: """))
         
         elif Question3 in ["A", "B", "C"]:
             print("Sadly you got it wrong but the next question will be easier!")
