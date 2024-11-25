f1f54d871653c3e4941c0a83b01638743c05611d
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2024-11-25 10:21:55 -0700

    Nested Conditionals Infographic

---
+++ My Code for Videos and Infographics/nested_conditionals.py
@@ -0,0 +1,12 @@
+#Max Holdaway
+
+Num1 = int(input("Give Number 1: "))
+
+Num2 = int(input("Give Number 2: "))
+
+if Num1 == 10:
+    print("One number is good.")
+    if Num2 == 20:
+        print("Both numbers are good.")
+else:
+    print("No numbers are good.")
